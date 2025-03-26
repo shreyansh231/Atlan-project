a978798cc1e93ec7f21af167a2314c05e4f8a609
Shreyansh <shreyanshagrawal1122@gmail.com>
2025-03-27 00:05:39 +0530

    side table added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,68 @@
-import logo from "./logo.svg";
 import { useState } from "react";
 import "./App.css";
+import TableStructure from "./table";
 
 function App() {
   const [inputQuery, setInputQuery] = useState("");
   const [ishoverQuery, setIshoverQuery] = useState(false);
-
+  const customers = require("./customers.json");
+  const suppliers = require("./suppliers.json");
+  const products = require("./products.json");
   return (
     <div className="App">
       <div className="App-sidebar">
         <div className="App-Suggestions">
           <p className="App-Suggestions-Title">Suggestions</p>
           <div className="App-Suggestions-List">
-            <p className="App-Suggestions-List-Item">
+            <p
+              className="App-Suggestions-List-Item"
+              onClick={() => setInputQuery("SELECT * FROM table_name;")}
+            >
               SELECT * FROM table_name;
             </p>
-            <p className="App-Suggestions-List-Item">
+            <p
+              className="App-Suggestions-List-Item"
+              onClick={() =>
+                setInputQuery("SELECT column1, column2 FROM table_name;")
+              }
+            >
               SELECT column1, column2 FROM table_name;
             </p>
-            <p className="App-Suggestions-List-Item">
+            <p
+              className="App-Suggestions-List-Item"
+              onClick={() =>
+                setInputQuery("SELECT * FROM table_name WHERE condition;")
+              }
+            >
               SELECT * FROM table_name WHERE condition;
             </p>
-            <p className="App-Suggestions-List-Item">
+            <p
+              className="App-Suggestions-List-Item"
+              onClick={() =>
+                setInputQuery("SELECT * FROM table_name ORDER BY column_name;")
+              }
+            >
               SELECT * FROM table_name ORDER BY column_name;
             </p>
-            <p className="App-Suggestions-List-Item">
+            <p
+              className="App-Suggestions-List-Item"
+              onClick={() => setInputQuery("SELECT * FROM table_name LIMIT 5;")}
+            >
               SELECT * FROM table_name LIMIT 5;
             </p>
-            <p className="App-Suggestions-List-Item">
+            <p
+              className="App-Suggestions-List-Item"
+              onClick={() =>
+                setInputQuery(
+                  "SELECT * FROM table_name WHERE condition ORDER BY column_name LIMIT 5;"
+                )
+              }
+            >
               SELECT * FROM table_name WHERE condition ORDER BY column_name
               LIMIT 5;
             </p>
           </div>
         </div>
-        <div className="App-tabledata"></div>
       </div>
       <div className="App-container">
         <header className="App-header">
@@ -53,6 +82,21 @@ function App() {
           <button className="button" onClick={() => console.log("Run Query")}>
             Run Query
           </button>
+        </div>
+      </div>
+      <div className="App-tabledata">
+        <p className="App-tabledata-Title">Table Data</p>
+        <div className="App-tabledata-List">
+          <TableStructure
+            tableHead={Object.keys(customers[0])}
+            tableName="customers"
+            tableNo={1}
+          />
+          <TableStructure
+            tableHead={Object.keys(products[0])}
+            tableName="products"
+            tableNo={2}
+          />
         </div>
       </div>
     </div>
