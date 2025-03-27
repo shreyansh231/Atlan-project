2ef9861044d36ae5e07f9766d277f6a84ebdc42f
Shreyansh <shreyanshagrawal1122@gmail.com>
2025-03-28 00:00:15 +0530

    final

--- src/Components/utils.js
+++ src/Components/utils.js
@@ -1,31 +1,35 @@
 export function getTableInfo(data) {
-    let infoStore = null;
-    if (data === 1) {
-      infoStore = require("../data/customers.json");
-    } else if (data === 2) {
-      infoStore = require("../data/products.json");
-    }
-    
-    let tableHeaders = Object.keys(infoStore[0]);
-    let tableRows = infoStore.map((row) => Object.values(row));
-    
-    return { tableHeaders, tableRows };
+  let infoStore = null;
+  if (data === 1) {
+    infoStore = require("../data/customers.json");
+  } else if (data === 2) {
+    infoStore = require("../data/products.json");
   }
-  
-  export function generateGraphData(data) {
-    let infoStore = null;
-    if (data === 1) {
-      infoStore = require("../data/customers.json");
-    } else if (data === 2) {
-      infoStore = require("../data/products.json");
+
+  let tableHeaders = Object.keys(infoStore[0]);
+  let tableRows = infoStore.map((row, index) => {
+    if (index !== 0) {
+      return Object.values(row);
     }
-    
-    let tableHeaders = Object.keys(infoStore[0]);
-    let tableRows = infoStore.map((row) => Object.values(row));
-    
-    return tableRows.map((row, index) => ({
-      name: tableHeaders[index],
-      value: Number(row[data !== 1 ? 6 : 7]),
-    }));
+    return null;
+  }).filter(row => row !== null);
+
+  return { tableHeaders, tableRows };
+}
+
+export function generateGraphData(data) {
+  let infoStore = null;
+  if (data === 1) {
+    infoStore = require("../data/customers.json");
+  } else if (data === 2) {
+    infoStore = require("../data/products.json");
   }
-  
+
+  let tableHeaders = Object.keys(infoStore[0]);
+  let tableRows = infoStore.map((row) => Object.values(row));
+
+  return tableRows.map((row, index) => ({
+    name: tableHeaders[index],
+    value: Number(row[data !== 1 ? 6 : 7]),
+  }));
+}
