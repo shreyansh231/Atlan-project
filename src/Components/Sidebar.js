4dde708e9863b1a2f894f37becadd46c42258981
Shreyansh <shreyanshagrawal1122@gmail.com>
2025-03-28 02:30:44 +0530

    code refractoring and improved ui

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,72 +1,54 @@
-const Sidebar = ({setInputQuery,setTableData}) => {
-    return (
-        <div className="App-sidebar">
-          <div className="App-Suggestions">
-            <p className="App-Suggestions-Title">Suggestions</p>
-            <div className="App-Suggestions-List">
-              <p
-                className="App-Suggestions-List-Item"
-                onClick={() => {
-                  setInputQuery("SELECT * FROM products;");
-                  setTableData(2);
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const Sidebar = ({ setInputQuery, setTableData }) => {
+  const queries = [
+    { query: "SELECT * FROM products;", tableData: 2 },
+    { query: "SELECT column1, column2 FROM table_name;", tableData: 1 },
+    { query: "SELECT * FROM customers WHERE condition;", tableData: 1 },
+    { query: "SELECT * FROM customers ORDER BY column_name;", tableData: 1 },
+    { query: "SELECT * FROM customers LIMIT 5;", tableData: 1 },
+    {
+      query:
+        "SELECT * FROM table_name WHERE condition ORDER BY column_name LIMIT 5;",
+      tableData: 1,
+    },
+  ];
+
+  return (
+    <div className="App-sidebar">
+      <div className="App-Suggestions">
+        <p className="App-Suggestions-Title">Suggestions</p>
+        <div className="App-Suggestions-List">
+          {queries.map((item, index) => (
+            <div
+              key={index}
+              className="App-Suggestions-List-Item"
+              onClick={() => {
+                setInputQuery(item.query);
+                setTableData(item.tableData);
+              }}
+            >
+              <SyntaxHighlighter
+                language="sql"
+                style={atomDark}
+                wrapLines={true}
+                wrapLongLines={true}
+                customStyle={{
+                  overflowX: "auto",  // Enables horizontal scrolling
+                  whiteSpace: "nowrap", // Prevents wrapping
+                  scrollbarWidth: "none", // Hides scrollbar
                 }}
+               
               >
-                SELECT * FROM products;
-              </p>
-              <p
-                className="App-Suggestions-List-Item"
-                onClick={() => {
-                  setInputQuery("SELECT column1, column2 FROM table_name;");
-                  setTableData(1);
-                }}
-              >
-                SELECT column1, column2 FROM table_name;
-              </p>
-              <p
-                className="App-Suggestions-List-Item"
-                onClick={() => {
-                  setInputQuery("SELECT * FROM customers WHERE condition;");
-                  setTableData(1);
-                }}
-              >
-                SELECT * FROM customers WHERE condition;
-              </p>
-              <p
-                className="App-Suggestions-List-Item"
-                onClick={() => {
-                  setInputQuery(
-                    "SELECT * FROM customers ORDER BY column_name;"
-                  );
-                  setTableData(1);
-                }}
-              >
-                SELECT * FROM customers ORDER BY column_name;
-              </p>
-              <p
-                className="App-Suggestions-List-Item"
-                onClick={() => {
-                  setInputQuery("SELECT * FROM customers LIMIT 5;");
-                  setTableData(1);
-                }}
-              >
-                SELECT * FROM customers LIMIT 5;
-              </p>
-              <p
-                className="App-Suggestions-List-Item"
-                onClick={() => {
-                  setInputQuery(
-                    "SELECT * FROM table_name WHERE condition ORDER BY column_name LIMIT 5;"
-                  );
-                  setTableData(1);
-                }}
-              >
-                SELECT * FROM table_name WHERE condition ORDER BY column_name
-                LIMIT 5;
-              </p>
+                {item.query}
+              </SyntaxHighlighter>
             </div>
-          </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
